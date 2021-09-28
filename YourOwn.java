a8fb6730e20db5932f9542e00c0b1abe49496c16
Sing Yee Harte <harte.s3821@gmail.com>
2021-09-28 17:30:01 -0400

    initial commit

---
+++ YourOwn.java
@@ -0,0 +1,12 @@
+// file name must be the same as the class name
+// every file IS a class
+public class YourOwn {
+    // the file or class with the exact method signature below is always the launching point of the application, you cannot run a java program without.
+    public static void main(String[] args) {
+        // for now, all code we run goes in here.
+        System.out.println("Name: Sing Yee");
+        System.out.println("Age: 28");
+        System.out.println("Hometown: Brooklyn");
+    }
+
+}
